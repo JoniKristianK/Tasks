faebbea4171f88423cd5d27090247c57d766a409
JoniKristianK <joni.karilainen@gmail.com>
2021-10-07 07:37:00 +0300

    Fix: bug

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
-	@Input() text: string;
-	@Input() color: string;
+	@Input()
+	text: string | undefined;
+	@Input()
+	color: string | undefined;
 	@Output() btnClick = new EventEmitter();
 
 	constructor() {}
